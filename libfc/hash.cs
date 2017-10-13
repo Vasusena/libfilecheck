df0d977dcd9973e4bd0d8da4440b2fb4a0b8b581
vasusena <harshpatel96@live.com>
2017-10-13 14:55:41 -0400

    created the projct skeleton

---
+++ libfc/hash.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace libfc
+{
+    public class Hash
+    {
+        //Write a function to return Sha1 hash for the contentes of the textfile
+        public static string getMd5(String path)
+        {
+            return "hash";
+        }
+
+
+        //Write a function to return Sha1 hash for the contentes of the textfile
+        public string GetSha1(String path)
+        {
+            return "hash";
+        }
+
+        //Write a function to calculate md5 hash of the given file
+
+        public string CheckMd5(String path)
+        {
+            return "hash";
+        }
+
+        //Write a function to calculate Sha1 hash of the given file
+        public string CheckSha1(String path)
+        {
+            return "hash";
+        }
+
+    }
+}
---
+++ libfilecheck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using libfc;
+
+namespace libfilecheck
+{
+
+    //this is test class that will work as a testclass to test changes you made in the libfs
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(Hash.getMd5("test"));
+
+            Console.ReadKey();
+        }
+    }
+}
